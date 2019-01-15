ae238d6a8f4982374558b6b10cbbbd5bd18ea4df
Renan Diego <renan.ccomp@gmail.com>
2019-01-15 18:19:03 -0300

    Page Header

--- frontend/src/processos/processos.jsx
+++ frontend/src/processos/processos.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Processo extends Component {
     render(){
         return(
             <div>
-                <h1>Processo</h1>
+                <PageHeader name='Processos' small='Cadastro'></PageHeader>
             </div>
         )
     }
